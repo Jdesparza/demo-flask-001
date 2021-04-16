ba0ee6da6adaf00411ea59d47c84b62824e69d21
jdesparza <jdesparza@utpl.edu.ec>
2021-04-16 12:06:57 -0500

    agregacion de /listado al .py

--- run.py
+++ run.py
@@ -9,12 +9,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return "<h1>Hola mundo con Flask</h1>"
+	return "<h1>¡Hola mundo! :)</h1>"
+	return "<h2>Por: Jdesparza</h2>"
 
 @app.route('/suma')
 def suma():
-    resultado = 10 + 10
-    return "<h3>10 + 10 = %d</h3>" % (resultado)
+	resultado = 10 + 10
+	return "<h3>10 + 10 = %d</h3>" % (resultado)
+
+@app.route('/listado')
+def listado():
+	provincias = ["Esmeraldas", "Manabí", "Los Ríos", "Santa Elena", "Guayas"]
+	capitales = ["Esmeraldas", "Portoviejo", "Babahoyo", "Santa Elena", "Guayaquil"]
+
+	ecuador = ""
+
+	for provincia, capital in zip (provincias, capitales):
+		ecuador = "%s<h3>Provincia= %s - Capital= %s\n</h3>" % (ecuador, provincia, capital)
+	
+	return ecuador 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
+	app.run(host='127.0.0.1', port=8000, debug=True)
